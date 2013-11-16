572e52f53ff8d77e2ce2a6dd254f5a868e34add4
Kevin Murray <k.d.murray.91@gmail.com>
2013-11-16 22:36:34 +1100

    update kmtest.h

--- include/kmtest.h
+++ include/kmtest.h
@@ -16,8 +16,14 @@
  * =====================================================================================
  */
 
+/*
+TODO: document usage of this header and make more TEST_X macros or find a way to generalise them
+    - also, shorten back down to 79 chars wide
+*/
 
 #include "kmbase.h"
+/* Everyone loves a maze. A lovely macro maze! */
+/* Set tab to be 4 spaces or it will look like shit. Sorry Linus */
 
 #define TEST(RETVAL, EXPECTED, NAME, FORMAT)                                                    \
     __kmtest_tests++;                                                                           \
@@ -35,16 +41,28 @@
 #define TEST_INT(RETVAL, EXPECTED, NAME) TEST(RETVAL, EXPECTED, NAME, "%i")
 #define TEST_SIZET(RETVAL, EXPECTED, NAME) TEST(RETVAL, EXPECTED, NAME, "%zu")
 #define TEST_PTR(RETVAL, EXPECTED, NAME) TEST(RETVAL, EXPECTED, NAME, "%p")
-#define TEST_STR(RETVAL, EXPECTED, NAME) TEST(RETVAL, EXPECTED, NAME, "%s")
+#define TEST_STR(RETVAL, EXPECTED, NAME)                                                        \
+    __kmtest_tests++;                                                                           \
+    if (strcmp(RETVAL, EXPECTED) != 0) {                                                        \
+        fprintf(stderr,                                                                         \
+                "%-*s failed:\n\tgot '%s', expected '%s' from '" #RETVAL "'\n",                 \
+                KMLIB_LINEWIDTH-7, NAME, RETVAL, EXPECTED);                                     \
+        __kmtest_failures++;                                                                    \
+    }                                                                                           \
+    else {                                                                                      \
+        fprintf(stdout, "%-*s passed\n", KMLIB_LINEWIDTH-7, NAME);                              \
+    }
+
+
 
 
 #define __TEST_VARS                                                                             \
     static int __kmtest_failures = 0;                                                           \
-    static int __kmtest_tests = 0;                                                              \
+    static int __kmtest_tests = 0;
 
 
 #ifdef  KMLIB_DEBUG
-#define	__TEST_SELFTEST                                                                       \
+#define	__TEST_SELFTEST                                                                         \
 static void test_testers() {                                                                    \
     printf("Testing kmtest macros. Every second test should fail\n\n");                         \
     /* test TEST macros */                                                                      \
@@ -55,11 +73,15 @@ static void test_testers() {                                                    
     TEST_SIZET((size_t)1, (size_t)1, "TEST_SIZET")                                              \
     TEST_SIZET((size_t)2, (size_t)1, "TEST_SIZET")                                              \
                                                                                                 \
-    TEST_PTR(&"test", &"test", "TEST_PTR")                                                      \
-    TEST_PTR(&"test", NULL, "TEST_PTR")                                                         \
+    void *p1 = malloc(1);                                                                       \
+    void *p2 = malloc(1);                                                                       \
+    TEST_PTR(p1, p1, "TEST_PTR")                                                                \
+    TEST_PTR(p1, p2, "TEST_PTR")                                                                \
+    free(p1); p1=NULL;                                                                          \
+    free(p2); p2=NULL;                                                                          \
                                                                                                 \
-    TEST_PTR(&"test", &"test", "TEST_PTR")                                                      \
-    TEST_PTR(&"test", NULL, "TEST_PTR")                                                         \
+    TEST_STR("test", "test", "TEST_PTR")                                                        \
+    TEST_STR("test", "", "TEST_PTR")                                                            \
                                                                                                 \
     printf("End of kmtest.h self testing. No more tests should fail\n");                        \
     __kmtest_failures -= 4; /* UPDATE THIS NUMBER!!!!!  */                                      \
