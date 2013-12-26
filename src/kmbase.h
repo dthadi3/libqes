6acca7202a3b217745770ae5e126be2543e0843e
Kevin Murray <k.d.murray.91@gmail.com>
2013-12-26 23:55:41 +1100

    A lot of changes to kmbase.h
    - move all external includes to kmbase.h
    - make zip macros
    - add a few more convenience macros

--- src/kmbase.h
+++ src/kmbase.h
@@ -6,11 +6,13 @@
  *    Description:  Base shared functions for all kmlib modules
  *
  *        Version:  1.0
- *        Created:  12/11/13 13:08:43
+ *        Created:  11/08/13 21:34:38
  *       Revision:  none
+ *        License:  GPLv3+
  *       Compiler:  gcc
  *
- *         Author:  Kevin Murray
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *                                [include word penguin in subject]
  *
  * ===========================================================================
  */
@@ -21,22 +23,48 @@
 /* #####   HEADER FILE INCLUDES   ########################################## */
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <strings.h>
+#include <stddef.h>
+#include <stdint.h>
 
+#include "kmlib_config.h"
 
 /* #####   EXPORTED MACROS   ############################################### */
 /* constants */
+
+#define	__FP_TYPE_GZIP			/*  */
+
+
+/* Definitions to make changing fp type easy */
+#ifdef  __FP_TYPE_GZIP
+#   include <zlib.h>
+#   define	__FP_TYPE gzFile
+#   define	__FP_READ gzread
+#   define	__FP_ERR gzerror
+#   define	__FP_OPEN gzopen
+#   define	__FP_CLOSE gzclose
+#endif     /* -----  not __KMFILE_TYPE_GZIP  ----- */
+
 
 /* Leave this undefined and define with with `gcc -DKMLIB_DEBUG`
 #define                     KMLIB_DEBUG
 */
 #define                     KMLIB_LINEWIDTH     79
 
-
 /* helpers */
 #define __ERROR(MSG)        fprintf(stderr, MSG);
-#define FATAL_ERROR(MSG)    ERROR(MSG) exit(EXIT_FAILURE);
+#define FATAL_ERROR(MSG)    __ERROR(MSG) exit(EXIT_FAILURE);
 
 #define KM_ERROR(MSG)       fprintf(stderr, "%s\n\tIn %s at %i in %s\n",      \
                                     MSG, __func__, __LINE__, __FILE__);
+
+#define ALLOC_ERROR fprintf(stderr, "Failed to allocate memory\n");           \
+    exit(EXIT_FAILURE);
+#define CHECK_ALLOC(pointer) if(pointer == NULL)                              \
+    { ALLOC_ERROR; printf("allocating bytes at ln %i\n", __LINE__);}
+#define FREE(pointer) if(pointer != NULL) {free(pointer); pointer = NULL;}    \
+    else {KM_ERROR("attempt to free null pointer")}
+
 
 #endif /* KMBASE_H */
