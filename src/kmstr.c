de1470ecbe9b24aae6f394448271a56f47ac81b1
Kevin Murray <spam@kdmurray.id.au>
2014-05-15 20:07:13 +1000

    [kmstr] update with semi-stable api & some docs

--- src/kmstr.c
+++ src/kmstr.c
@@ -21,46 +21,80 @@
 inline int
 str_ok (const str_t *str)
 {
-    return (str != NULL && str->s != NULL && str->m > 0 && str->l > 0);
+    return (str != NULL && str->s != NULL && str->m > 0);
 }
+
 
 inline void
 print_str (const str_t *str, FILE *stream)
 {
-    if (str_ok(str)) {
+    if (km_likely(str_ok(str))) {
         fprintf(stream, "%s", str->s);
     }
 }
 
 inline void
-print_strz (const str_t *str, zfile_t *stream)
+destroy_str_cp (str_t *str)
 {
-    KM_ZWRITE(stream->fp, str->s, str->l);
+    km_free(str->s);
 }
 
 inline void
 destroy_str (str_t *str)
 {
-    km_free(str->s);
+    destroy_str_cp(str);
     km_free(str);
 }
 
-inline str_t *
-str_from_charptr (const char *cp)
+inline int
+str_nullify (str_t *str)
 {
-    size_t len = strlen(cp);
-    str_t *str = create_str(len + 1); /* Enough space for cp + '\0' */
+    if (km_unlikely(str == NULL)) return 0;
+    bzero(str->s, str->m);
+    str->l = 0;
+    return 1;
+}
+
+inline int
+str_fill_charptr_len (str_t *str, const char *cp, const size_t len)
+{
+    if (km_unlikely(str == NULL) || km_unlikely(cp == NULL)) return 0;
+    if (km_unlikely(str->m < len + 1)) {
+        while (str->m < len + 1) {
+            str->m = kmroundupz(str->m);
+        }
+        str->s = km_realloc(str->s, str->m * sizeof(*str->s));
+    }
     memcpy(str->s, cp, len);
+    str->s[len] = '\0';
     str->l = len;
-    return str;
+    return 1;
+}
+
+inline int
+str_fill_charptr (str_t *str, const char *cp)
+{
+    size_t len = 0;
+    if (km_unlikely(!str_ok(str)) || km_unlikely(cp == NULL)) return 0;
+    len = strlen(cp);
+    return str_fill_charptr_len(str, cp, len);
+}
+
+inline void
+init_str (str_t *str, size_t capacity)
+{
+    if (km_unlikely(str == NULL)) return;
+    str->l = 0;
+    str->s = km_calloc(capacity, sizeof(*str->s));
+    str->m = capacity;
 }
 
 inline str_t *
-create_str (size_t len)
+create_str (size_t capacity)
 {
-    str_t *str = km_calloc(1, sizeof(*str), errprintexit);
-    str->l = 0;
-    str->s = km_calloc(__INIT_LINE_LEN, sizeof(*str->s), errprintexit);
-    str->m = __INIT_LINE_LEN;
+    str_t *str = km_calloc(1, sizeof(*str));
+    /* We don't worry about NULL here. init_str will return before derefencing
+       and we'll return NULL below. */
+    init_str(str, capacity);
     return str;
 }
--- src/kmstr.h
+++ src/kmstr.h
@@ -20,7 +20,6 @@
 #define KMSTR_H
 
 #include <kmutil.h>
-#include <kmzfile.h>
 
 typedef struct _str {
     char *s;
@@ -28,11 +27,66 @@ typedef struct _str {
     size_t m;
 } str_t;
 
-extern void print_str (const str_t *str, FILE *stream);
-extern void print_strz (const str_t *str, zfile_t *stream);
-extern void destroy_str (str_t *str);
-extern str_t *str_from_charptr (const char *cp);
-extern str_t *create_str (size_t len);
+
+/*===  FUNCTION  ============================================================*
+Name:           str_ok
+Parameters:     const str_t *str: `str_t` ref to check.
+Description:    Checks if a string is OK to use, i.e. checks that `str->s` is
+                a valid `char` array.
+Returns:        An `int` that evaluates to true or false.
+ *===========================================================================*/
 extern int str_ok (const str_t *str);
+
+
+/*===  FUNCTION  ============================================================*
+Name:           create_str
+Parameters:     size_t len: Initial capacity of `str_t`.
+Description:    Create a `str_t` on the heap with room for `len` characters.
+Returns:        `str_t *` Pointer to a memory address.
+ *===========================================================================*/
+extern str_t *create_str (size_t len);
+
+/*===  FUNCTION  ============================================================*
+Name:           init_str
+Parameters:     str_t *str: String to initialise (normally on the stack).
+                size_t len: Initial capacity of `str_t`.
+Description:    Initialise a `str_t` (typically on the stack) with room for
+                `len` characters.
+Returns:        void
+ *===========================================================================*/
+extern void init_str (str_t *str, size_t len);
+
+
+extern int str_fill_charptr_len (str_t *str, const char *cp,
+        const size_t len);
+extern int str_fill_charptr (str_t *str, const char *cp);
+
+/*===  FUNCTION  ============================================================*
+Name:           str_nullify
+Paramters:      str_t *str: `str_t` to nullify.
+Description:    Invalidates a `str_t` without freeing the `char []`.
+Returns:        int: True or False, indicating success or failure.
+ *===========================================================================*/
+extern int str_nullify(str_t *str);
+
+extern void print_str (const str_t *str, FILE *stream);
+
+/*===  FUNCTION  ============================================================*
+Name:           destroy_str
+Paramters:      str_t *: `str_t` to destroy.
+Description:    Frees `str->s` and the str_t struct itself.
+Returns:        void
+ *===========================================================================*/
+extern void destroy_str (str_t *str);
+
+/*===  FUNCTION  ============================================================*
+Name:           destroy_str_cp
+Paramters:      str_t *: String to destrop
+Description:    Frees `str->s` without freeing the str_t struct itself. For use
+                on `str_t`s allocated on the stack.
+Returns:        void
+ *===========================================================================*/
+extern void destroy_str_cp (str_t *str);
+
 
 #endif /* KMSTR_H */
