d156effd88b3197c4ad2b2236bf52bbc0adab7c5
Kevin Murray <spam@kdmurray.id.au>
2014-05-15 20:11:10 +1000

    [testdata.h] remove static chars from testdata

--- test/testdata.h
+++ test/testdata.h
@@ -19,26 +19,13 @@
 #ifndef TESTDATA_H
 #define TESTDATA_H
 
-static const size_t n_loremipsum_lines = 11;
-static const size_t loremipsum_line_lens[] = {
-    80, 76, 80, 75, 80, 79, 77, 75, 69, 1, 20
-};
-static const char *loremipsum_lines[] = {
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ornare tortor et\n",
-    "rhoncus iaculis. Sed suscipit, arcu nec elementum vestibulum, tortor tortor\n",
-    "dictum dui, eu sodales magna orci eu libero. Cras commodo, ligula tempor auctor\n",
-    "vulputate, eros urna gravida eros, eget congue leo quam quis mi. Curabitur\n",
-    "luctus augue nibh, eget vehicula augue commodo eget. Donec condimentum molestie\n",
-    "adipiscing. In non purus lacus. Nam nec mollis mauris. Donec rhoncus, diam sit\n",
-    "amet rhoncus viverra, lectus risus tincidunt ipsum, in dignissim justo purus\n",
-    "eget enim. Fusce congue nulla egestas est auctor faucibus. Integer feugiat\n",
-    "molestie leo, a interdum neque pretium nec. Etiam sit amet nibh leo.\n",
-    "\n",
-    "End of lorem ipsum.\n",
-};
-
+extern const size_t n_loremipsum_lines;
+extern const size_t loremipsum_line_lens[];
+extern const char *loremipsum_lines[];
 extern char *data_prefix;
 extern char *out_prefix;
+extern char *text_file;
+extern char *gz_text_file;
 extern char *fasta_file;
 extern char *fastq_file;
 extern char *gzfastq_file;
