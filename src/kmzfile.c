9bdea5e96024838ed63c2c3ceac644e0a14f3b6e
Kevin Murray <spam@kdmurray.id.au>
2014-05-18 14:47:39 +1000

    [kmzfile] add updated zfile module

--- src/kmzfile.c
+++ src/kmzfile.c
@@ -19,11 +19,13 @@
 #include "kmzfile.h"
 
 zfile_t *
-zfopen_ (const char *path, const char *mode, errhandler_t onerr, char *file,
-        int line)
+zfopen_ (const char *path, const char *mode, errhandler_t onerr,
+        const char *file, int line)
 {
     /* create file struct */
-    zfile_t *zf = km_calloc(1, sizeof(*zf), onerr);
+    zfile_t *zf = NULL;
+    if (path == NULL || mode == NULL) return NULL;
+    zf = km_calloc(1, sizeof(*zf));
     /* Open file, handling any errors */
     zf->fp = KM_ZOPEN(path, mode);
     if (zf->fp == NULL) {
@@ -32,6 +34,7 @@ zfopen_ (const char *path, const char *mode, errhandler_t onerr, char *file,
         km_free(zf);
         return(NULL);
     }
+    zf->mode = zfile_guess_mode(mode);
     /* Use a larger than default IO buffer, speeds things up */
     KM_ZBUFFER(zf->fp, KM_FILEBUFFER_LEN);
     /* init struct fields */
@@ -41,16 +44,37 @@ zfopen_ (const char *path, const char *mode, errhandler_t onerr, char *file,
     return(zf);
 }
 
+inline int
+zfile_guess_mode (const char *mode)
+{
+    if (mode[0] == 'r') {
+        return RW_READ;
+    } else if(mode[0] == 'w' || mode[0] == 'a') {
+        return RW_WRITE;
+    }
+    return RW_UNKNOWN;
+}
+
+inline void
+zfrewind (zfile_t *file)
+{
+    if (zfile_ok(file)) {
+        KM_ZSEEK(file->fp, 0, SEEK_SET);
+        file->filepos = 0;
+        file->eof = 0;
+    }
+}
+
 void
-zfclose (zfile_t *file)
+zfclose_ (zfile_t *file)
 {
     if (file != NULL) {
         if (file->fp != NULL) {
             KM_ZCLOSE(file->fp);
         }
         km_free(file->path);
+        km_free(file);
     }
-    km_free(file);
 }
 
 inline ssize_t
@@ -58,11 +82,11 @@ zfreadline_realloc_ (zfile_t *file, char **buf, size_t *size,
               errhandler_t onerr, const char *src, const int line)
 {
     size_t len = 0;
-    if (buf == NULL ||  file == NULL || size == NULL) {
+    if (buf == NULL || file == NULL || size == NULL) {
         return -2; /* EOF is normally == -1, so use -2 to differentiate them */
     }
     if (*buf == NULL) {
-        buf = km_calloc_(__INIT_LINE_LEN, sizeof(*buf), onerr, src, line);
+        *buf = km_malloc_(__INIT_LINE_LEN * sizeof(*buf), onerr, src, line);
         *size = __INIT_LINE_LEN;
     }
     while(((*buf)[len] = KM_ZFGETC(file->fp)) != EOF && (*buf)[len++] != '\n') {
@@ -87,19 +111,21 @@ zfreadline_realloc_ (zfile_t *file, char **buf, size_t *size,
     }
 }
 
-inline ssize_t zfreadline (zfile_t *file, char **dest, size_t maxlen)
+inline ssize_t
+zfreadline (zfile_t *file, char *dest, size_t maxlen)
 {
     size_t len = 0;
-    if (dest == NULL || *dest == NULL || file == NULL) {
+    int next = 0;
+    if (dest == NULL || file == NULL || maxlen < 1) {
         /* EOF is normally == -1, so use -2 to differentiate them */
         return -2;
     }
-    while ( ((*dest)[len] = KM_ZFGETC(file->fp)) != EOF && \
-            (*dest)[len] != '\n' && \
-            len < maxlen) {
-        len++;
+
+    while ( (next = KM_ZFGETC(file->fp)) != EOF && len < maxlen - 1) {
+        dest[len++] = next;
+        if (next == '\n') break;
     }
-    (*dest)[len] = '\0';
+    dest[len] = '\0';
     file->filepos += len;
     if (KM_ZEOF(file->fp)) {
         file->eof = 1;
@@ -107,4 +133,43 @@ inline ssize_t zfreadline (zfile_t *file, char **dest, size_t maxlen)
     } else {
         return len;
     }
+}
+
+inline ssize_t
+zfreadline_str (zfile_t *file, str_t *str)
+{
+    if (file == NULL || str_ok(str)) {
+        return -2; /* ERROR, not EOF */
+    }
+    return zfreadline_realloc(file, &str->s, &str->m);
+}
+
+inline void
+zfprint_str (zfile_t *stream, const str_t *str)
+{
+    KM_ZWRITE(stream->fp, str->s, str->l);
+}
+
+inline int
+zfile_ok(const zfile_t *zf)
+{
+    return  zf != NULL && \
+            zf->fp != NULL;
+}
+
+inline int
+zfile_readable(const zfile_t *zf)
+{
+    return zfile_ok(zf) && !zf->eof;
+
+}
+
+inline int
+zfpeek (zfile_t *file)
+{
+    int chr = '\0';
+    if (km_unlikely(!zfile_ok(file))) return -2;
+    chr = KM_ZFGETC(file->fp);
+    KM_ZFUNGETC(chr, file->fp);
+    return chr;
 }
--- src/kmzfile.h
+++ src/kmzfile.h
@@ -19,15 +19,15 @@
 #ifndef KMZFILE_H
 #define KMZFILE_H
 
-
 #include <kmutil.h>
+#include <kmstr.h>
 
 typedef struct __zfile_t {
     KM_ZTYPE fp;
     char *path;
     int eof;
     off_t filepos;
-    char *mode;
+    rwmode_t mode;
 } zfile_t;
 
 /* zfopen:
@@ -35,7 +35,7 @@ typedef struct __zfile_t {
     `onerr`
  */
 zfile_t *zfopen_ (const char *path, const char *mode, errhandler_t onerr,
-        char *file, int line);
+        const char *file, int line);
 #define	zfopen(pth, mod) \
     zfopen_(pth, mod, KM_DEFAULT_ERR_FN, __FILE__, __LINE__)
 #define	zfopen_errnil(pth, mod) \
@@ -45,16 +45,25 @@ zfile_t *zfopen_ (const char *path, const char *mode, errhandler_t onerr,
 #define	zfopen_errprintexit(pth, mod) \
     zfopen_(pth, mod, errprintexit, __FILE__, __LINE__)
 
+
+extern int zfile_guess_mode (const char *mode);
+extern int zfpeek (zfile_t *file);
+extern void zfrewind (zfile_t *file);
 /* zfclose:
-    Destroy a `zfile_t` without leaking memory.
+    Destroy a `zfile_t` without leaking memory, and set `file` to NULL.
  */
-void zfclose (zfile_t *file);
+void zfclose_ (zfile_t *file);
+#define zfclose(file) do {                      \
+            zfclose_ (file);                    \
+            file = NULL;                        \
+        } while(0)
 
 /* zfreadline:
     Read a line from `file` into a `char *` pointed to by `buf`, copying at
     most `maxlen` charachters into `*buf` INCLUDING the terminating '\0'.
  */
-extern ssize_t zfreadline (zfile_t *file, char **dest, size_t maxlen);
+extern ssize_t zfreadline (zfile_t *file, char *dest, size_t maxlen);
+extern ssize_t zfreadline_str (zfile_t *file, str_t *str);
 
 /* ===  FUNCTION  =============================================================
           Name: zfreadline_realloc
@@ -68,13 +77,16 @@ extern ssize_t zfreadline (zfile_t *file, char **dest, size_t maxlen);
  * ==========================================================================*/
 extern ssize_t zfreadline_realloc_ (zfile_t *file, char **buf, size_t *size,
               errhandler_t onerr, const char *src, const int line);
-#define km_readline_realloc(fp, buf, sz) \
-    km_readline_realloc_(fp, buf, sz, KM_DEFAULT_ERR_FN, __FILE__, __LINE__)
-#define km_readline_realloc_errnil(fp, buf, sz) \
-    km_readline_realloc_(fp, buf, sz, errnil, __FILE__, __LINE__)
-#define km_readline_realloc_errprint(fp, buf, sz) \
-    km_readline_realloc_(fp, buf, sz, errprint, __FILE__, __LINE__)
-#define km_readline_realloc_errprintexit(fp, buf, sz) \
-    km_readline_realloc_(fp, buf, sz, errprintexit, __FILE__, __LINE__)
+#define zfreadline_realloc(fp, buf, sz) \
+    zfreadline_realloc_(fp, buf, sz, KM_DEFAULT_ERR_FN, __FILE__, __LINE__)
+#define zfreadline_realloc_errnil(fp, buf, sz) \
+    zfreadline_realloc_(fp, buf, sz, errnil, __FILE__, __LINE__)
+#define zfreadline_realloc_errprint(fp, buf, sz) \
+    zfreadline_realloc_(fp, buf, sz, errprint, __FILE__, __LINE__)
+#define zfreadline_realloc_errprintexit(fp, buf, sz) \
+    zfreadline_realloc_(fp, buf, sz, errprintexit, __FILE__, __LINE__)
 
+extern void zfprint_str (zfile_t *stream, const str_t *str);
+extern int zfile_ok(const zfile_t *zf);
+extern int zfile_readable(const zfile_t *zf);
 #endif /* KMZFILE_H */
