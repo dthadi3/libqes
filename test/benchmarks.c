10ea3a6bb0f5d95d4ba2330df2d2e5c312a8c915
Kevin Murray <spam@kdmurray.id.au>
2014-05-21 19:37:11 +1000

    [benchmarks] perform many trials of each benchmark

--- test/benchmarks.c
+++ test/benchmarks.c
@@ -26,11 +26,11 @@ static const char *infile;
 
 typedef struct __bench {
     char *name;
-    void (*fn)();
+    void (*fn)(int silent);
 } bench_t;
 
 void
-zfreadline_realloc_file()
+zfreadline_realloc_file(int silent)
 {
     size_t bsz = 1<<4;
     char *buf = malloc(bsz);
@@ -40,12 +40,13 @@ zfreadline_realloc_file()
     while ((len = zfreadline_realloc(file, &buf, &bsz)) != EOF) {
         flen += len;
     }
-    printf("[zfreadline_realloc]\tFile of %zu chars\n", flen);
+    if (!silent)
+        printf("[zfreadline_realloc]\tFile of %zu chars\n", flen);
     zfclose(file);
 }
 
 void
-zfreadline_file()
+zfreadline_file(int silent)
 {
     size_t bsz = 1<<10;
     char buf[bsz];
@@ -55,12 +56,13 @@ zfreadline_file()
     while ((len = zfreadline(file, buf, bsz)) != EOF) {
         flen += len;
     }
-    printf("[zfreadline]\t\tFile of %zu chars\n", flen);
+    if (!silent)
+        printf("[zfreadline]\t\tFile of %zu chars\n", flen);
     zfclose(file);
 }
 
 void
-gnu_getline_file()
+gnu_getline_file(int silent)
 {
     size_t bsz = 1<<4;
     char *buf = malloc(bsz);
@@ -70,7 +72,8 @@ gnu_getline_file()
     while ((len = getline(&buf, &bsz, file)) != EOF) {
         flen += len;
     }
-    printf("[getline]\t\tFile of %zu chars\n", flen);
+    if (!silent)
+        printf("[getline]\t\tFile of %zu chars\n", flen);
     fclose(file);
 }
 static const bench_t benchmarks[] = {
@@ -83,25 +86,33 @@ static const bench_t benchmarks[] = {
 int
 main (int argc, char *argv[])
 {
+    bench_t thisbench;
+    clock_t start, end;
+    size_t nbench = 0;
+    int n_rounds = 10;
     if (argc > 1) {
         infile = strdup(argv[1]);
     } else {
         fprintf(stderr, "USAGE:\nbench <file>\n\n");
         return EXIT_FAILURE;
     }
-    bench_t thisbench;
-    clock_t start, end;
-    size_t nbench = 0;
+    if (argc > 2) {
+        n_rounds = atoi(argv[2]);
+    }
     while (1) {
+        int iii;
         thisbench = benchmarks[nbench++];
         if (!(thisbench.name && thisbench.fn)) {
             break;;
         }
         start = clock();
-        (*thisbench.fn)();
+        for (iii = 0; iii<n_rounds; iii++) {
+            (*thisbench.fn)(iii != n_rounds - 1);
+        }
         end = clock();
-        printf("Test %s took %0.3fs\n\n", thisbench.name,
-                (float)(end - start) / (float)CLOCKS_PER_SEC);
+        printf("Test %s took %0.6fs per round [%d rounds]\n\n", thisbench.name,
+                (float)(end - start) / (float)(CLOCKS_PER_SEC * n_rounds),
+                n_rounds);
     }
 
     return EXIT_SUCCESS;
