52996037edb374c107c6e50d6de1634a1d1133a4
Kevin Murray <spam@kdmurray.id.au>
2014-06-03 18:44:21 +1000

    [kmseq] fixes from  refactor of str_fill_charptr
    + small style changes

--- src/kmseq.c
+++ src/kmseq.c
@@ -23,6 +23,7 @@ seq_t *
 create_seq ()
 {
     seq_t *seq = km_malloc(sizeof(*seq));
+
     init_str(&seq->name, __INIT_LINE_LEN);
     init_str(&seq->comment, __INIT_LINE_LEN);
     init_str(&seq->seq, __INIT_LINE_LEN);
@@ -34,6 +35,7 @@ seq_t *
 create_seq_no_qual ()
 {
     seq_t *seq = km_malloc(sizeof(*seq));
+
     init_str(&seq->name, __INIT_LINE_LEN);
     init_str(&seq->comment, __INIT_LINE_LEN);
     init_str(&seq->seq, __INIT_LINE_LEN);
@@ -116,7 +118,7 @@ inline int
 seq_fill_name (seq_t *seqobj, const char *name, size_t len)
 {
     if (name != NULL) {
-        str_fill_charptr_len(&seqobj->name, name, len);
+        str_fill_charptr(&seqobj->name, name, len);
         return 1;
     }
     return 0;
@@ -126,7 +128,7 @@ inline int
 seq_fill_comment (seq_t *seqobj, const char *comment, size_t len)
 {
     if (comment != NULL) {
-        str_fill_charptr_len(&seqobj->comment, comment, len);
+        str_fill_charptr(&seqobj->comment, comment, len);
         return 1;
     }
     return 0;
@@ -136,7 +138,7 @@ inline int
 seq_fill_seq (seq_t *seqobj, const char *seq, size_t len)
 {
     if (seq != NULL) {
-        str_fill_charptr_len(&seqobj->seq, seq, len);
+        str_fill_charptr(&seqobj->seq, seq, len);
         return 1;
     }
     return 0;
@@ -146,7 +148,7 @@ inline int
 seq_fill_qual (seq_t *seqobj, const char *qual, size_t len)
 {
     if (qual != NULL) {
-        str_fill_charptr_len(&seqobj->qual, qual, len);
+        str_fill_charptr(&seqobj->qual, qual, len);
         return 1;
     }
     return 0;
@@ -159,16 +161,22 @@ seq_fill_header (seq_t *seqobj, const char *header, size_t len)
         char *tmp = strchr(header, ' ');
         if (tmp != NULL) {
             tmp[0] = '\0';
-            str_fill_charptr_len(&seqobj->name, header, tmp - header);
-            str_fill_charptr(&seqobj->comment, tmp + 1);
+            str_fill_charptr(&seqobj->name, header, tmp - header);
+            str_fill_charptr(&seqobj->comment, tmp + 1, 0);
         } else {
-            str_fill_charptr(&seqobj->name, header);
+            str_fill_charptr(&seqobj->name, header, len);
             str_nullify(&seqobj->name);
         }
     }
     return 1;
 }
 
+/*===  FUNCTION  ============================================================*
+Name:           destroy_seq
+Paramters:      seq_t *: seq to destroy.
+Description:    Deallocate and set to NULL a seq_t on the heap.
+Returns:        void.
+ *===========================================================================*/
 void
 destroy_seq_ (seq_t *seq)
 {
