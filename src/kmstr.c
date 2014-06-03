44e96732f2c49e44f6c50a1b0a485f7e4c3fc809
Kevin Murray <spam@kdmurray.id.au>
2014-06-03 18:42:30 +1000

    [kmstr] small fixes/style changes in kmstr

--- src/kmstr.c
+++ src/kmstr.c
@@ -21,7 +21,7 @@
 inline int
 str_ok (const str_t *str)
 {
-    return (str != NULL && str->s != NULL && str->m > 0);
+    return str != NULL && str->s != NULL && str->m > 0;
 }
 
 
@@ -36,7 +36,7 @@ print_str (const str_t *str, FILE *stream)
 inline void
 destroy_str_cp (str_t *str)
 {
-    km_free(str->s);
+    if (km_unlikely(str != NULL)) km_free(str->s);
 }
 
 inline void
@@ -88,6 +88,7 @@ inline str_t *
 create_str (size_t capacity)
 {
     str_t *str = km_calloc(1, sizeof(*str));
+
     /* We don't worry about NULL here. init_str will return before derefencing
        and we'll return NULL below. */
     init_str(str, capacity);
