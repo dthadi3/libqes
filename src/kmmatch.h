0c7978fcc512f39d500a4e43eb96bb6d7ab27688
Kevin Murray <spam@kdmurray.id.au>
2014-06-03 19:06:13 +1000

    [kmmatch] add docs in header

--- src/kmmatch.h
+++ src/kmmatch.h
@@ -20,12 +20,29 @@
 #ifndef KMMATCH_H
 #define KMMATCH_H
 
-
-/* #####   HEADER FILE INCLUDES   ########################################## */
 #include <kmutil.h>
 
-/* #####   EXPORTED FUNCTION DECLARATIONS   ################################ */
+
+/*===  FUNCTION  ============================================================*
+Name:           hamming
+Paramters:      const char *seq1, *seq2: Two strings to compare.
+Description:    Find the hamming distance between two strings. The strings are
+                matched until the length of the smallest string.
+Returns:        The hamming distance between ``seq1`` and ``seq2``.
+ *===========================================================================*/
 extern size_t hamming(const char *seq1, const char *seq2);
+
+
+/*===  FUNCTION  ============================================================*
+Name:           hamming_max
+Paramters:      const char *seq1, *seq2: Two strings to compare.
+                size_t max: Stop counting at ``max``, return ``max + 1``.
+Description:    Find the hamming distance between two strings. The strings are
+                matched until the length of the smallest string, or until the
+                maximum hamming distance (``max``) is reached.
+Returns:        The hamming distance between ``seq1`` and ``seq2``, or
+                ``max + 1`` if the hamming distance exceeds ``max``.
+ *===========================================================================*/
 extern size_t hamming_max(const char *seq1, const char *seq2, size_t max);
 
 #endif /* KMMATCH_H */
