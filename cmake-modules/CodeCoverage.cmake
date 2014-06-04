df9042767d74452f9c9fdf8272584a0d37c9e2cf
Kevin Murray <spam@kdmurray.id.au>
2014-06-04 17:34:22 +1000

    [CodeCoverage] always enable branch coverage
    lcov>=1.10 has it disabled by default, and it's really helpful.

--- cmake-modules/CodeCoverage.cmake
+++ cmake-modules/CodeCoverage.cmake
@@ -101,15 +101,15 @@ FUNCTION(SETUP_TARGET_FOR_COVERAGE _targetname _testrunner _outputname)
 	ADD_CUSTOM_TARGET(${_targetname}
 
 		# Cleanup lcov
-		${LCOV_PATH} --directory . --zerocounters
+		${LCOV_PATH} --rc lcov_branch_coverage=1 --directory . --zerocounters
 
 		# Run tests
 		COMMAND ${_testrunner} ${ARGV3}
 
 		# Capturing lcov counters and generating report
-		COMMAND ${LCOV_PATH} --directory . --capture --output-file ${_outputname}.info
-		COMMAND ${LCOV_PATH} --remove ${_outputname}.info 'test/*' '/usr/*' --output-file ${_outputname}.info.cleaned
-		COMMAND ${GENHTML_PATH} -o ${_outputname} ${_outputname}.info.cleaned
+		COMMAND ${LCOV_PATH} --rc lcov_branch_coverage=1 --directory . --capture --output-file ${_outputname}.info
+		COMMAND ${LCOV_PATH} --rc lcov_branch_coverage=1 --remove ${_outputname}.info 'test/*' '/usr/*' --output-file ${_outputname}.info.cleaned
+		COMMAND ${GENHTML_PATH} --branch-coverage --function-coverage -o ${_outputname} ${_outputname}.info.cleaned
 		COMMAND ${CMAKE_COMMAND} -E remove ${_outputname}.info ${_outputname}.info.cleaned
 
 		WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
