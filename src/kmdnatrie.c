4d0ff080ee7350f6914f71e119578520593480f2
Kevin Murray <spam@kdmurray.id.au>
2014-06-05 11:36:35 +1000

    [kmdnatrie] merge stash conflict !BROKEN!

--- src/kmdnatrie.c
+++ src/kmdnatrie.c
@@ -16,25 +16,93 @@
  * ============================================================================
  */
 
+#include "kmdnatrie.h"
+
+unsigned char char_2na_table[256] = {
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  3, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 0, 4, 1,  4, 4, 4, 2,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  3, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,
+    4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4,  4, 4, 4, 4
+};
+
+#define NT_IDX(nt, res, onfail) STMT_BEGIN      \
+    res = char_2na_table[nt];                   \
+    if (res > 3) {                              \
+        onfail;                                 \
+    } STMT_END
+
 dnatrie_t *
-dnatrie_create ()
+dnatrie_create (void)
 {
     dnatrie_t *trie = km_calloc(1, sizeof(*trie));
-    trie->alloced_nodes = 1<<10;
-    trie->nodes = km_calloc(trie->alloced_nodes, size_t(*trie->nodes));
+    trie->nodes_alloced = 1<<10;
+    trie->nodes = km_calloc(trie->nodes_alloced, sizeof(*trie->nodes));
     trie->next_node = trie->nodes;
 
 }
-void
-dnatrie_insert(dnatrie_t *trie, const char *seq)
+
+int
+dnatrie_insert(dnatrie_t *trie, const char *seq, size_t len,
+        dnatrie_node_data_t data)
+{
+    dnatrie_node_t *last_nt_in_trie;
+    dnatrie_node_t *iter;
+    dnatrie_node_t *next;
+    size_t iii = 0;
+    size_t prefix_in_tree = 0;
+
+    iter = dnatrie_lookup(trie, seq, len, &prefix_in_tree);
+    while (prefix_in_tree < len) {
+        size_t child_num = 0;
+        /* Always save this, but only increment the next node if we need it */
+        next = trie->next_node;
+        /* Reserve us a new node for the next nt */
+        trie->next_node += 1;
+        /* Check for resizing, do so if necessary */
+        if (trie->nodes - iter <= 1) {
+            trie->nodes_alloced = kmroundupz(trie->nodes_alloced);
+            trie->nodes = km_realloc(trie->nodes,
+                    trie->nodes_alloced * sizeof(trie->nodes));
+            if (trie->nodes == NULL) {
+                return 0;
+            }
+        }
+        iter->children[
+    }
+}
+
+dnatrie_node_t *
+dnatrie_lookup(dnatrie_t *trie, const char *seq, size_t len,
+               size_t *prefix_len)
 {
     dnatrie_node_t *iter = trie->nodes;
-    while (iter != NULL) {
-        iter = 
+    dnatrie_node_t *parent = iter;
+    size_t iii = 0;
+    unsigned char child
+    if (len == 0) {
+        len = strlen(seq);
+    }
+    while (iter != NULL && iii < len) {
+        size_t res = 0;
+        parent = iter;
+        NT_IDX(seq[iii++], res, *prefix_len = iii; return NULL;)
+        iter = iter->children[res];
     }
 
-}
-size_t dnatrie_lookup(dnatrie_t *trie, const char *seq);
+
+
 void dnatrie_remove(dnatrie_t *trie, const char *seq);
 extern size_t char_to_nt_offset(const char nt);
 
--- src/kmdnatrie.h
+++ src/kmdnatrie.h
@@ -23,24 +23,26 @@
 #include <kmutil.h>
 #include <kmstr.h>
 
-typedef struct __dnatrie_node_t {
-    struct __dnatrie_node_t *children[4];
-    union {
-        size_t count;
-        void *data
-    } data;
+typedef union __dnatrie_node_data {
+    size_t count;
+    void *ptr;
+} dnatrie_node_data_t;
+
+typedef struct __dnatrie_node {
+    struct __dnatrie_node *children[4];
+    dnatrie_node_data_t data;
 } dnatrie_node_t;
 
 
-tyepdef struct __dnatrie_t {
+typedef struct __dnatrie {
     dnatrie_node_t *nodes;
     dnatrie_node_t *next_node;
     size_t nodes_alloced;
-}
+} dnatrie_t;
 
-dnatrie_t *dnatrie_create();
-void dnatrie_insert(dnatrie_t *trie, const char *seq);
-size_t dnatrie_lookup(dnatrie_t *trie, const char *seq);
+dnatrie_t *dnatrie_create(void);
+int dnatrie_insert(dnatrie_t *trie, const char *seq, size_t len);
+dnatrie_node_t *dnatrie_lookup(dnatrie_t *trie, const char *seq, size_t len, size_t *prefix_len);
 void dnatrie_remove(dnatrie_t *trie, const char *seq);
 extern size_t char_to_nt_offset(const char nt);
 
