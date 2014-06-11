074c1f7e81738b7b23c0a4d56744039eac0ab8fa
Kevin Murray <spam@kdmurray.id.au>
2014-06-11 22:00:58 +1000

    [benchmarks] rewrite main loop of benchmarks
    Also rename all benchmark functions

--- test/benchmarks.c
+++ test/benchmarks.c
@@ -27,12 +27,12 @@
 
 #include "kseq.h"
 
-void zfreadline_realloc_file(int silent);
-void zfreadline_file(int silent);
-void gnu_getline_file(int silent);
-void seqfile_parse_fq(int silent);
-void kseq_parse_fq(int silent);
-void seqfile_write(int silent);
+void bench_zfreadline_realloc_file(int silent);
+void bench_zfreadline_file(int silent);
+void bench_gnu_getline_file(int silent);
+void bench_seqfile_parse_fq(int silent);
+void bench_kseq_parse_fq(int silent);
+void bench_seqfile_write(int silent);
 
 
 KSEQ_INIT(gzFile, gzread)
@@ -46,7 +46,7 @@ typedef struct __bench {
 } bench_t;
 
 void
-zfreadline_realloc_file(int silent)
+bench_zfreadline_realloc_file(int silent)
 {
     size_t bsz = 1<<4;
     char *buf = malloc(bsz);
@@ -65,7 +65,7 @@ zfreadline_realloc_file(int silent)
 }
 
 void
-zfreadline_file(int silent)
+bench_zfreadline_file(int silent)
 {
     size_t bsz = 1<<10;
     char buf[bsz];
@@ -82,7 +82,7 @@ zfreadline_file(int silent)
 }
 
 void
-gnu_getline_file(int silent)
+bench_gnu_getline_file(int silent)
 {
     size_t bsz = 1<<4;
     char *buf = malloc(bsz);
@@ -154,18 +154,19 @@ bench_seqfile_parse_fq(int silent)
     if (!silent) {
         printf("[seqfile_fq] Total seq len %zu\n", seq_len);
     }
-    destroy_seqfile(sf);
+    seqfile_destroy(sf);
     seq_destroy(seq);
 }
 
 void
-kseq_parse_fq(int silent)
+bench_kseq_parse_fq(int silent)
 {
     gzFile fp = gzopen(infile, "r");
     kseq_t *seq = kseq_init(fp);
     ssize_t res = 0;
     size_t n_recs = 0;
     size_t seq_len = 0;
+
     while ((res = kseq_read(seq)) >= 0) {
         seq_len += res;
         n_recs++;
@@ -178,7 +179,7 @@ kseq_parse_fq(int silent)
 }
 
 void
-seqfile_write(int silent)
+bench_seqfile_write(int silent)
 {
     seq_t *seq = seq_create();
     ssize_t res = 0;
@@ -200,19 +201,20 @@ seqfile_write(int silent)
     if (!silent) {
         printf("[seqfile_write] Total file len %zu to %s\n", res, fname);
     }
-    destroy_seqfile(sf);
+    seqfile_destroy(sf);
     seq_destroy(seq);
     remove(fname);
 
 }
 
 static const bench_t benchmarks[] = {
-    { "zfreadline", &zfreadline_file},
-    { "zfreadline_realloc", &zfreadline_realloc_file},
-    { "gnu_getline", &gnu_getline_file},
-    { "seqfile_parse_fq", &seqfile_parse_fq},
-    { "kseq_parse_fq", &kseq_parse_fq},
-    { "seqfile_write", &seqfile_write},
+    { "zfreadline", &bench_zfreadline_file},
+    { "zfreadline_realloc", &bench_zfreadline_realloc_file},
+    { "gnu_getline", &bench_gnu_getline_file},
+    { "seqfile_parse_fq", &bench_seqfile_parse_fq},
+    { "seqfile_par_iter_fq", &bench_seqfile_par_iter_fq},
+    { "kseq_parse_fq", &bench_kseq_parse_fq},
+    { "seqfile_write", &bench_seqfile_write},
     { NULL, NULL}
 };
 
@@ -221,31 +223,55 @@ main (int argc, char *argv[])
 {
     bench_t thisbench;
     clock_t start, end;
+    size_t iii = 0;
+    int rnds = 0;
     size_t nbench = 0;
-    int n_rounds = 10;
-    if (argc > 1) {
-        infile = strdup(argv[1]);
-    } else {
-        fprintf(stderr, "USAGE:\nbench <file>\n\n");
+
+    if (argc == 1) {
+        fprintf(stderr, "USAGE:\nbench <file> <rounds> <bench> [<bench> ...]\n\n");
+        fprintf(stderr, "\nAvalilable benchmarks are:\n");
+        nbench = 0;
+        while (1) {
+            thisbench = benchmarks[nbench++];
+            if ((thisbench.name && thisbench.fn)) {
+                fprintf(stderr, "%s\n", thisbench.name);
+            } else {
+                return EXIT_FAILURE;
+            }
+        }
+    }
+    if (argc < 4) {
+        fprintf(stderr, "USAGE:\nbench <file> <rounds> <bench> [<bench> ...]\n\n");
         return EXIT_FAILURE;
+    } else {
+        infile = strdup(argv[1]);
+        rnds = atoi(argv[2]);
+        printf("Begining benchmarks.\n");
+        printf("---------------------------------------------------------------------\n");
     }
-    if (argc > 2) {
-        n_rounds = atoi(argv[2]);
-    }
-    while (1) {
-        int iii;
-        thisbench = benchmarks[nbench++];
-        if (!(thisbench.name && thisbench.fn)) {
-            break;;
+    for (iii = 3; iii < argc; iii++) {
+        nbench = 0;
+        while (1) {
+            thisbench = benchmarks[nbench++];
+            if (!(thisbench.name && thisbench.fn)) {
+                fprintf(stderr, "bad benchmark %s\n", argv[iii]);
+                break;
+            }
+            if  (strcmp(argv[iii], thisbench.name) == 0) {
+                int rnd = 0;
+                start = clock();
+                for (rnd = 0; rnd<rnds; rnd++) {
+                    (*thisbench.fn)(rnd != rnds - 1);
+                }
+                end = clock();
+                printf("Benchmark %s took %0.6fs per round [%d rounds]\n",
+                        thisbench.name,
+                        (float)(end - start) / (float)(CLOCKS_PER_SEC * rnds),
+                        rnds);
+                printf("---------------------------------------------------------------------\n");
+                break;
+            }
         }
-        start = clock();
-        for (iii = 0; iii<n_rounds; iii++) {
-            (*thisbench.fn)(iii != n_rounds - 1);
-        }
-        end = clock();
-        printf("Test %s took %0.6fs per round [%d rounds]\n\n", thisbench.name,
-                (float)(end - start) / (float)(CLOCKS_PER_SEC * n_rounds),
-                n_rounds);
     }
     free(infile);
     return EXIT_SUCCESS;
