c594d5efa2d1606c2e2bddcf4211f44b31da0a1b
Kevin Murray <spam@kdmurray.id.au>
2014-06-12 15:37:55 +1000

    [kmseqfile] add seqfile iterator macros
    These macros setup serial or parallelised iterator loops that loop thru
    a fast[aq] file, and write the per-sequence loop yourself. Less overhead
    and more flexibility than the seqfile_iter_X function, that work with
    function pointers. These haven't been removed yet though, not sure what
    I'll do there.

--- src/kmseqfile.h
+++ src/kmseqfile.h
@@ -98,6 +98,125 @@ void seqfile_destroy_(seqfile_t *seqfile);
             seqfile_destroy_(seqfile);                                      \
             seqfile = NULL;                                                 \
         } while(0)
+
+#define SEQFILE_ITER_PARALLEL_SINGLE_BEGIN(fle, sq, ln, opts)               \
+    _Pragma(STRINGIFY(omp parallel shared(fle) opts default(none)))         \
+    {                                                                       \
+        seq_t *sq = seq_create();                                           \
+        ssize_t ln = 0;                                                     \
+        while(1) {                                                          \
+            _Pragma(STRINGIFY(omp critical))                                \
+            {                                                               \
+                ln = seqfile_read(fle, sq);                                 \
+            }                                                               \
+            if (ln < 0) {                                                   \
+                break;                                                      \
+            }
+
+#define SEQFILE_ITER_PARALLEL_SINGLE_END(sq)                                \
+        }                                                                   \
+        seq_destroy(sq);                                                    \
+    }
+
+#define SEQFILE_ITER_PARALLEL_PAIRED_BEGIN(fle1, fle2, sq1, sq2, ln1, ln2, opts)\
+    _Pragma(STRINGIFY(omp parallel shared(fle1, fle2) opts default(none)))  \
+    {                                                                       \
+        seq_t *sq1 = seq_create();                                          \
+        seq_t *sq2 = seq_create();                                          \
+        ssize_t ln1 = 0;                                                    \
+        ssize_t ln2 = 0;                                                    \
+        while(1) {                                                          \
+            _Pragma(STRINGIFY(omp critical))                                \
+            {                                                               \
+                ln1 = seqfile_read(fle1, sq1);                              \
+                ln2 = seqfile_read(fle2, sq2);                              \
+            }                                                               \
+            if (ln1 < 0 || ln2 < 0) {                                       \
+                break;                                                      \
+            }
+
+#define SEQFILE_ITER_PARALLEL_PAIRED_END(sq1, sq2)                          \
+        }                                                                   \
+        seq_destroy(sq1);                                                   \
+        seq_destroy(sq2);                                                   \
+    }
+
+#define SEQFILE_ITER_PARALLEL_INTERLEAVED_BEGIN(fle, sq1, sq2, ln1, ln2, opts)\
+    _Pragma(STRINGIFY(omp parallel shared(fle) opts default(none)))         \
+    {                                                                       \
+        seq_t *sq1 = seq_create();                                          \
+        seq_t *sq2 = seq_create();                                          \
+        ssize_t ln1 = 0;                                                    \
+        ssize_t ln2 = 0;                                                    \
+        while(1) {                                                          \
+            _Pragma(STRINGIFY(omp critical))                                \
+            {                                                               \
+                ln1 = seqfile_read(fle, sq1);                               \
+                ln2 = seqfile_read(fle, sq2);                               \
+            }                                                               \
+            if (ln1 < 0 || ln2 < 0) {                                       \
+                break;                                                      \
+            }
+
+#define SEQFILE_ITER_PARALLEL_INTERLEAVED_END(sq1, sq2)                     \
+        }                                                                   \
+        seq_destroy(sq1);                                                   \
+        seq_destroy(sq2);                                                   \
+    }
+
+#define SEQFILE_ITER_SINGLE_BEGIN(fle, sq, ln)                              \
+    {                                                                       \
+        seq_t *sq = seq_create();                                           \
+        ssize_t ln = 0;                                                     \
+        while(1) {                                                          \
+            ln = seqfile_read(fle, sq);                                     \
+            if (ln < 0) {                                                   \
+                break;                                                      \
+            }
+
+#define SEQFILE_ITER_SINGLE_END(sq)                                         \
+        }                                                                   \
+        seq_destroy(sq);                                                    \
+    }
+
+#define SEQFILE_ITER_PAIRED_BEGIN(fle1, fle2, sq1, sq2, ln1, ln2)           \
+    {                                                                       \
+        seq_t *sq1 = seq_create();                                          \
+        seq_t *sq2 = seq_create();                                          \
+        ssize_t ln1 = 0;                                                    \
+        ssize_t ln2 = 0;                                                    \
+        while(1) {                                                          \
+            ln1 = seqfile_read(fle1, sq1);                                  \
+            ln2 = seqfile_read(fle2, sq2);                                  \
+            if (ln1 < 0 || ln2 < 0) {                                       \
+                break;                                                      \
+            }
+
+#define SEQFILE_ITER_PAIRED_END(sq1, sq2)                                   \
+        }                                                                   \
+        seq_destroy(sq1);                                                   \
+        seq_destroy(sq2);                                                   \
+    }
+
+#define SEQFILE_ITER_INTERLEAVED_BEGIN(fle, sq1, sq2, ln1, ln2)             \
+    {                                                                       \
+        seq_t *sq1 = seq_create();                                          \
+        seq_t *sq2 = seq_create();                                          \
+        ssize_t ln1 = 0;                                                    \
+        ssize_t ln2 = 0;                                                    \
+        while(1) {                                                          \
+            ln1 = seqfile_read(fle, sq1);                                   \
+            ln2 = seqfile_read(fle, sq2);                                   \
+            if (ln1 < 0 || ln2 < 0) {                                       \
+                break;                                                      \
+            }
+
+#define SEQFILE_ITER_INTERLEAVED_END(sq1, sq2)                              \
+        }                                                                   \
+        seq_destroy(sq1);                                                   \
+        seq_destroy(sq2);                                                   \
+    }
+
 /*
  * Collection of options to a seqfile_iter function.
  */
