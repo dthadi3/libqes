614fde68fe81e9c33f37e39fdd975abbb1810e1d
Kevin Murray <spam@kdmurray.id.au>
2015-01-11 12:12:06 +1100

    Add initial versions of qes_log module

---
+++ src/qes_log.c
@@ -0,0 +1,195 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  qes_log.c
+ *    Description:  Logging module of libngs2
+ *        License:  GPLv3+
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+
+#include "qes_log.h"
+
+struct qes_logger *
+qes_logger_create(void)
+{
+    return qes_calloc(1, sizeof(struct qes_logger));
+}
+
+int
+qes_logger_init(struct qes_logger  *logger,
+                const char         *name,
+                enum qes_log_level  level)
+{
+    if (logger == NULL) return 1;
+
+    if (name != NULL) {
+        logger->name = strdup(name);
+    }
+    logger->level = level;
+    return 0;
+}
+
+int
+qes_logger_add_destination_struct(struct qes_logger *logger,
+                                  struct qes_log_destination *destination)
+{
+    struct qes_log_destination *new = NULL;
+    size_t new_sz = logger->n_destinations + 1;
+
+    new = qes_realloc(logger->destinations,
+                      new_sz * sizeof(*logger->destinations));
+    if (new == NULL) {
+        return 1;
+    }
+    memcpy(&new[new_sz - 1], destination, sizeof(*new));
+    logger->destinations = new;
+    logger->n_destinations = new_sz;
+    return 0;
+}
+
+int
+qes_logger_add_destination(struct qes_logger *logger, FILE *stream,
+                           enum qes_log_level level)
+{
+    struct qes_log_destination *new = NULL;
+    size_t new_sz = logger->n_destinations + 1;
+
+    new = qes_realloc(logger->destinations,
+                      new_sz * sizeof(*logger->destinations));
+    if (new == NULL) {
+        return 1;
+    }
+    logger->destinations = new;
+    logger->n_destinations = new_sz;
+    /* For ease of refernce below, save the ptr to the unallocated final
+     * struct*/
+    new = &new[new_sz - 1];
+    new->stream = stream;
+    new->level = level;
+    return 0;
+}
+
+void
+_qes_logger_destroy(struct qes_logger *logger)
+{
+    if (logger != NULL) {
+        qes_free(logger->name);
+        /* Memory leak, but the API needs to change */
+        qes_free(logger->destinations);
+    }
+}
+
+struct qes_log_entry *
+qes_log_entry_create(void)
+{
+    return qes_calloc(1, sizeof(struct qes_log_entry));
+}
+
+int
+qes_log_entry_init(struct qes_log_entry *entry, enum qes_log_level level,
+                   const char *message)
+{
+    if (entry == NULL || message == NULL) return -1;
+
+    entry->level = level;
+    entry->message = strdup(message);
+    return 0;
+}
+
+
+struct qes_log_entry *
+qes_log_entry_format_va(enum qes_log_level level, const char *format,
+                        va_list args)
+{
+    int res = 0;
+    char *message = NULL;
+    struct qes_log_entry *entry;
+
+
+    /* Format the error message w/ user input */
+    res = vasprintf(&message, format, args);
+    if (res < 1) {
+        /* Alloc inside vasprintf failed */
+        return NULL;
+    }
+    /* Make the entry struct */
+    entry = qes_log_entry_create();
+    res = qes_log_entry_init(entry, level, message);
+    if (res != 0) {
+        /* Init failed */
+        qes_log_entry_destroy(entry);
+        return NULL;
+    }
+    return entry;
+}
+
+struct qes_log_entry *
+qes_log_entry_format(enum qes_log_level level, const char *format, ...)
+{
+    va_list args;
+    struct qes_log_entry *entry;
+
+
+    /* Format the error message w/ user input */
+    va_start(args, format);
+    entry = qes_log_entry_format_va(level, format, args);
+    va_end(args);
+    return entry;
+}
+
+void
+_qes_log_entry_destroy(struct qes_log_entry *entry)
+{
+    if (entry != NULL) {
+        qes_free(entry->message);
+        qes_free(entry);
+    }
+}
+
+static int
+_qes_logger_write_entry(struct qes_logger *logger, struct qes_log_entry *entry)
+{
+    size_t iii;
+    int res;
+
+    if (logger == NULL || entry == NULL) return 1;
+
+    /* Message is to unimportant for this logger */
+    if (logger->level > entry->level) return 0;
+
+    for (iii = 0; iii < logger->n_destinations; iii++) {
+        struct qes_log_destination *dest = &logger->destinations[iii];
+
+        /* Message is to unimportant for this destination */
+        if (dest->level > entry->level) continue;
+
+        res = fprintf(dest->stream, "%s", entry->message);
+        if (res < 0) return 1;
+    }
+    return 0;
+}
+
+int
+qes_log_message(struct qes_logger *logger, enum qes_log_level level,
+                const char *message)
+{
+    struct qes_log_entry *entry;
+
+    entry = qes_log_entry_format(level, "%s", message);
+    return _qes_logger_write_entry(logger, entry);
+}
+
+int
+qes_log_format(struct qes_logger *logger, enum qes_log_level level,
+               const char *format, ...)
+{
+    struct qes_log_entry *entry;
+    va_list args;
+
+    va_start(args, format);
+    entry = qes_log_entry_format_va(level, format, args);
+    va_end(args);
+    return _qes_logger_write_entry(logger, entry);
+}
---
+++ src/qes_log.h
@@ -0,0 +1,105 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  qes_log.h
+ *    Description:  Logging module of libngs2
+ *        License:  GPLv3+
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+
+#ifndef QES_LOG_H
+#define QES_LOG_H
+
+#include <stdlib.h>
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <qes_util.h>
+
+enum qes_log_level {
+    /* The idea is that the user can add values inbetween these, if they need
+     * to. a la Python's logging module. */
+    QES_LOG_DEBUG = 0,
+    QES_LOG_INFO = 10,
+    QES_LOG_WARNING = 20,
+    QES_LOG_ERROR = 30,
+    QES_LOG_FATAL = 40,
+};
+#define QES_LOG_LEVEL_FIRST_USER 41
+
+typedef enum qes_log_level QesLogLevel;
+
+struct qes_log_destination {
+    FILE *stream;
+    enum qes_log_level level;
+};
+typedef struct qes_log_destination QesLogDestination;
+
+struct qes_logger {
+    struct qes_log_destination *destinations;
+    size_t n_destinations;
+    enum qes_log_level level;
+    char *name;
+    int lock;
+};
+typedef struct qes_logger QesLogger;
+
+struct qes_log_entry {
+    char *message;
+    enum qes_log_level level;
+};
+typedef struct qes_log_entry QesLogEntry;
+
+
+struct qes_logger *qes_logger_create(void);
+int qes_logger_init(struct qes_logger *logger, const char *name,
+                    enum qes_log_level level);
+int qes_logger_add_destination_struct(struct qes_logger *logger,
+                                      struct qes_log_destination *destination);
+int qes_logger_add_destination(struct qes_logger *logger, FILE *stream,
+                               enum qes_log_level level);
+void _qes_logger_destroy(struct qes_logger *logger);
+#define qes_logger_destroy(l) ({ _qes_logger_destroy(l); l = NULL; })
+
+
+struct qes_log_entry *qes_log_entry_create(void);
+int qes_log_entry_init(struct qes_log_entry *entry, enum qes_log_level level,
+                       const char *message);
+struct qes_log_entry *qes_log_entry_format(enum qes_log_level level,
+                                           const char *format, ...);
+struct qes_log_entry *qes_log_entry_format_va(enum qes_log_level level,
+                                           const char *format, va_list args);
+void _qes_log_entry_destroy(struct qes_log_entry *log_entry);
+#define qes_log_entry_destroy(l) ({ _qes_log_entry_destroy(l); l = NULL; })
+
+
+int qes_log_message(struct qes_logger *logger, enum qes_log_level level,
+                    const char *message);
+#ifndef NDEBUG
+#define qes_log_message_debug(log, msg) qes_log_message(log, QES_LOG_DEBUG, msg)
+#else
+#define qes_log_message_debug(log, msg)
+#endif
+#define qes_log_message_info(log, msg) qes_log_message(log, QES_LOG_INFO, msg)
+#define qes_log_message_error(log, msg) qes_log_message(log, QES_LOG_ERROR, msg)
+#define qes_log_message_fatal(log, msg) qes_log_message(log, QES_LOG_FATAL, msg)
+
+
+int qes_log_format(struct qes_logger *logger, enum qes_log_level level,
+                   const char *format, ...);
+#ifndef NDEBUG
+#define qes_log_format_debug(log, fmt, ...) \
+        qes_log_format(log, QES_LOG_DEBUG, fmt, _VA_ARGS_)
+#else
+#define qes_log_format_debug(log, fmt, ...)
+#endif
+#define qes_log_format_info(log, fmt, ...) \
+        qes_log_format(log, QES_LOG_INFO, fmt, _VA_ARGS_)
+#define qes_log_format_error(log, fmt, ...) \
+        qes_log_format(log, QES_LOG_ERROR, fmt, _VA_ARGS_)
+#define qes_log_format_fatal(log, fmt, ...) \
+        qes_log_format(log, QES_LOG_FATAL, fmt, _VA_ARGS_)
+
+
+#endif /* QES_LOG_H */
