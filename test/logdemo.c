e8b7baffe36d7ab489a26dea9ab535c15780c8d8
Kevin Murray <spam@kdmurray.id.au>
2015-02-06 23:41:57 +1100

    Make a nicer demo of logging

--- test/logdemo.c
+++ test/logdemo.c
@@ -9,29 +9,25 @@
  * ============================================================================
  */
 
-
 #include <qes_log.h>
-
 
 int
 main (int argc, char *argv[])
 {
-    const char *message = "Test!";
     struct qes_logger *logger = qes_logger_create();
 
-    if (argc > 1) {
-        message = argv[1];
-    }
+    (void) argc;
+    (void) argv;
 
     qes_logger_init(logger, "Test Logger", QES_LOG_DEBUG);
     qes_logger_add_destination_formatted(logger, stdout, QES_LOG_DEBUG,
                                          &qes_log_formatter_pretty);
 
-    qes_log_format_debug(logger, "%s\n", message);
-    qes_log_format_info(logger, "%s\n", message);
-    qes_log_format_warning(logger, "%s\n", message);
-    qes_log_format_error(logger, "%s\n", message);
-    qes_log_format_fatal(logger, "%s\n", message);
+    qes_log_message_debug(logger, "Debug message, nice and quiet\n");
+    qes_log_message_info(logger, "Informative message, clearer\n");
+    qes_log_message_warning(logger, "Warning message, pay attention!\n");
+    qes_log_message_error(logger, "Error message, something's gone wrong\n");
+    qes_log_message_fatal(logger, "Fatal message, I'm leaving now\n");
 
     qes_logger_destroy(logger);
     return EXIT_SUCCESS;
