c10749fd0f3171d14fcd8b29e7908fd1cffbeddb
Kevin Murray <spam@kdmurray.id.au>
2015-02-11 15:12:59 +1100

    Add crc32 from gnulib

---
+++ src/qes_libgnu.c
@@ -0,0 +1,13 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  qes_libgnu.c
+ *    Description:  Functions required from gnulib
+ *        License:  GPLv3+
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+#include <stdlib.h>
+
+#include "crc.c"
---
+++ src/qes_libgnu.h
@@ -0,0 +1,20 @@
+/*
+ * ============================================================================
+ *
+ *       Filename:  qes_libgnu.h
+ *    Description:  Functions required from gnulib
+ *        License:  GPLv3+
+ *         Author:  Kevin Murray, spam@kdmurray.id.au
+ *
+ * ============================================================================
+ */
+
+#ifndef QES_LIBGNU_H
+#define QES_LIBGNU_H
+
+/* This file and qes_libgnu.c are designed to allow us to keep the sources of
+ * the gnulib functions intact and in their original seperate form. */
+
+#include "crc.h"
+
+#endif /* QES_LIBGNU_H */
